c9bfdd6a2bd364c1dbb8364a33ef7554ed63685c
Scott Campbell <thisisscottcampbell@gmail.com>
2021-08-09 15:46:31 -0400

    base render

---
+++ demo/src/components/columns.js
@@ -0,0 +1,30 @@
+export const COLUMNS = [
+	{
+		Header: 'Id',
+		accessor: 'id',
+	},
+	{
+		Header: 'Company Name',
+		accessor: 'company_name',
+	},
+	{
+		Header: 'Account Holder',
+		accessor: 'account_holder',
+	},
+	{
+		Header: 'Email',
+		accessor: 'email',
+	},
+	{
+		Header: 'Phone',
+		accessor: 'phone',
+	},
+	{
+		Header: 'Start',
+		accessor: 'start_date',
+	},
+	{
+		Header: 'City',
+		accessor: 'city',
+	},
+];
