baf78154a963451c748dbcddad66cd3a8ee73ab8
Scott <scott.campbell@numerated.com>
2021-08-12 17:02:57 -0400

    3

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,20 +1,54 @@
 import React, { useState } from "react";
-// import Table from "./React_Table/components/Table";
-// import BlueprintTable from "./Blueprint/components/BlueprintTable";
+import Table from "./React_Table/components/Table";
+import BlueprintTable from "./Blueprint/components/BlueprintTable";
 import SelectionTable from "./EUI/components/SelectionTable";
 
 const App = () => {
-  // const [renderTable, setRenderTable] = useState(true);
+  const [renderReactTable, setRenderReactTable] = useState(false);
+  const [renderBluePrint, setRenderBluePrint] = useState(false);
+  const [renderEUITable, setRenderEUI] = useState(false);
+
+  const setRT = () => {
+    setRenderReactTable(true);
+    setRenderBluePrint(false);
+    setRenderEUI(false);
+  };
+
+  const setBP = () => {
+    setRenderReactTable(false);
+    setRenderBluePrint(true);
+    setRenderEUI(false);
+  };
+
+  const setEUI = () => {
+    setRenderReactTable(false);
+    setRenderBluePrint(false);
+    setRenderEUI(true);
+  };
+
   return (
     <div style={{ marginLeft: "25px", marginRight: "25px" }}>
-      {/* <button
+      <button
         style={{ background: "tomato", color: "whitesmoke" }}
-        onClick={() => setRenderTable(!renderTable)}
+        onClick={setRT}
       >
-        Toggle Table
+        React Table
       </button>
-      {renderTable ? <Table /> : <BlueprintTable />} */}
-      <SelectionTable />
+      <button
+        style={{ background: "blue", color: "whitesmoke" }}
+        onClick={setBP}
+      >
+        Blueprint Table
+      </button>
+      <button
+        style={{ background: "cyan", color: "whitesmoke" }}
+        onClick={setEUI}
+      >
+        EUI Table
+      </button>
+      {renderReactTable && <Table />}
+      {renderBluePrint && <BlueprintTable />}
+      {renderEUITable && <SelectionTable />}
     </div>
   );
 };
